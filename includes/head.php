50f5dea8208edf6cc976b87d5a02040d5aaf8754
Olua Parrot <phvcgdx@gmail.com>
2022-03-16 14:34:37 +0100

    started index page

---
+++ includes/head.php
@@ -0,0 +1,12 @@
+<head>
+    <meta charset="utf-8">
+
+    <!-- permettra d'obtenir la réelle taille d'écran avec les media queries -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Pokedex</title>
+
+    <!-- stylesheets -->
+    <link rel="stylesheet" href="styles/index.css">
+    <link rel="stylesheet" href="header_footer.css">
+</head>
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <?php include('includes/head.php'); ?>
+    <body>
+        <?php include('includes/header.php'); ?>
+        <main>
+            <img src="images/pikachu.png" alt="pikachu">
+            <h1>BIENVENUE SUR LE POKEDEX DE L'ESGI !</h1>
+        </main>
+        <?php include('includes/footer.php'); ?>
+    </body>
+</html>
