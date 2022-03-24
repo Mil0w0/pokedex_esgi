d940076cc522f858ecb1b0982df526ac5b259162
Milo <lhilderal@myges.fr>
2022-03-24 14:50:56 +0100

    added a missing title at collection.php

--- collection.php
+++ collection.php
@@ -6,8 +6,9 @@
     ?>
     <body>
         <?php include('includes/header.php'); ?>
+        <h1>TOUS LES POKEMONS</h1>
         <main class="collection-container">
-
+        
         <?php
 
         $q = 'SELECT nom, pv, attaque, defense, vitesse, image FROM pokemon ORDER BY nom' ; 
@@ -44,106 +45,7 @@
                 </div>
                 <img src="images/pikachu.png">
             </div>
-            <div class="collection-card">
-                <div class="collection-text">
-                    <h4>Aéromite</h4>
-                    <p>PV</p>
-                    <p>Attaque</p>
-                    <p>Défense</p>
-                    <p>Vitesse</p>
-                </div>
-                <img src="images/pikachu.png">
-            </div>
-            <div class="collection-card">
-                <div class="collection-text">
-                    <h4>Aéromite</h4>
-                    <p>PV</p>
-                    <p>Attaque</p>
-                    <p>Défense</p>
-                    <p>Vitesse</p>
-                </div>
-                <img src="images/pikachu.png">
-            </div>
-            <div class="collection-card">
-                <div class="collection-text">
-                    <h4>Aéromite</h4>
-                    <p>PV</p>
-                    <p>Attaque</p>
-                    <p>Défense</p>
-                    <p>Vitesse</p>
-                </div>
-                <img src="images/pikachu.png">
-            </div>
-            <div class="collection-card">
-                <div class="collection-text">
-                    <h4>Aéromite</h4>
-                    <p>PV</p>
-                    <p>Attaque</p>
-                    <p>Défense</p>
-                    <p>Vitesse</p>
-                </div>
-                <img src="images/pikachu.png">
-            </div>
-            <div class="collection-card">
-                <div class="collection-text">
-                    <h4>Aéromite</h4>
-                    <p>PV</p>
-                    <p>Attaque</p>
-                    <p>Défense</p>
-                    <p>Vitesse</p>
-                </div>
-                <img src="images/pikachu.png">
-            </div>
-            <div class="collection-card">
-                <div class="collection-text">
-                    <h4>Aéromite</h4>
-                    <p>PV</p>
-                    <p>Attaque</p>
-                    <p>Défense</p>
-                    <p>Vitesse</p>
-                </div>
-                <img src="images/pikachu.png">
-            </div>
-            <div class="collection-card">
-                <div class="collection-text">
-                    <h4>Aéromite</h4>
-                    <p>PV</p>
-                    <p>Attaque</p>
-                    <p>Défense</p>
-                    <p>Vitesse</p>
-                </div>
-                <img src="images/pikachu.png">
-            </div>
-            <div class="collection-card">
-                <div class="collection-text">
-                    <h4>Aéromite</h4>
-                    <p>PV</p>
-                    <p>Attaque</p>
-                    <p>Défense</p>
-                    <p>Vitesse</p>
-                </div>
-                <img src="images/pikachu.png">
-            </div>
-            <div class="collection-card">
-                <div class="collection-text">
-                    <h4>Aéromite</h4>
-                    <p>PV</p>
-                    <p>Attaque</p>
-                    <p>Défense</p>
-                    <p>Vitesse</p>
-                </div>
-                <img src="images/pikachu.png">
-            </div>
-            <div class="collection-card">
-                <div class="collection-text">
-                    <h4>Aéromite</h4>
-                    <p>PV</p>
-                    <p>Attaque</p>
-                    <p>Défense</p>
-                    <p>Vitesse</p>
-                </div>
-                <img src="images/pikachu.png">
-            </div> -->
+             -->
         </main>
         <?php include('includes/footer.php'); ?>
     </body>
